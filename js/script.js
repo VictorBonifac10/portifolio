b06f9cc0982bd814f507b07266e6cbe272d558ef
victorbonifac10 <victor.alvesbonifacio@gmail.com>
2025-05-25 09:24:53 -0300

    Desenvolvimento do html, css e javascript do projeto

---
+++ js/script.js
@@ -0,0 +1,71 @@
+let dark = false
+const back = document.querySelector("body")
+const log = document.querySelector("#logo")
+const nameLogo = document.querySelector("#name")
+const linkHeader = document.querySelectorAll(".link-top-header")
+const botao = document.querySelector("#dark-light")
+const img = document.querySelector("#img-darklight")
+const paragraph = document.querySelector("p")
+const subTitle = document.querySelector("#subtitle")
+const titlePrincipal = document.querySelector("#element")
+const iconSocial = document.querySelectorAll(".box")
+
+
+function darkMode() {
+    if (dark === false) {
+        //liga o modo escuro
+
+        back.style.background = "#272737" //background
+
+        log.src = "./assets/img/yellow-logo.svg" //logo
+        nameLogo.style.color = "#fff"
+
+        linkHeader.forEach(linkHeader => { //header
+            linkHeader.style.color = "#ffdf56"
+        });
+
+        botao.style.color = "#fff"
+        botao.style.border = "1.5px solid #ffdf56"
+
+        img.src = "./assets/img/yellow-rocket.svg" //img
+
+        paragraph.style.color = "#fff" //textos
+        subTitle.style.color = "#fff"
+        element.style.color = "#ffdf56"
+
+        iconSocial.forEach(iconSocial => { //icones
+            iconSocial.style.background = "#ffdf56"
+        });
+
+        dark = true; // agora estamos no modo claro
+
+    } else {
+        // VOLTA para o modo claro
+        back.style.backgroundColor = "#ffffff";
+        log.src = "./assets/img/blue-logo.svg";
+        nameLogo.style.color = "#3c3c54";
+
+        linkHeader.forEach(link => {
+            link.style.color = "#0059ad";
+        });
+
+        botao.style.color = "#3c3c54"
+        botao.style.border = "1.5px solid #0059ad"
+
+        img.src = "./assets/img/blue-rocket.svg";
+
+        paragraph.style.color = "#3c3c54";
+        subTitle.style.color = "#3c3c54";
+        titlePrincipal.style.color = "#0059ad";
+
+        iconSocial.forEach(icon => {
+            icon.style.backgroundColor = "#0059ad";
+        });
+
+        dark = false; // agora estamos no modo escuro
+    }
+
+}
+
+
+ 
---
+++ js/typewriterEffect.js
@@ -0,0 +1,4 @@
+    var typed = new Typed('#element', {
+      strings: ['Bem-vindo ao meu portifólio', 'Bem-vindo ao meu portifólio'],
+      typeSpeed: 70,
+    });
