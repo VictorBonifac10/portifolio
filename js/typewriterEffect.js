1b569574d90f1e5fac2183141eef60b1df22c441
victorbonifac10 <victor.alvesbonifacio@gmail.com>
2025-08-09 20:17:02 -0300

    Remodelação visual dos elementos e outras seções

--- js/typewriterEffect.js
+++ js/typewriterEffect.js
@@ -1,4 +1,9 @@
-    var typed = new Typed('#element', {
-      strings: ['Bem-vindo ao meu portifólio', 'Bem-vindo ao meu portifólio'],
-      typeSpeed: 70,
-    });
+const typed = new Typed('#element', {
+  strings: ['Bem-vindo ao meu portifólio', 'Bem-vindo ao meu portifólio'],
+  typeSpeed: 40,
+});
+
+const move  = new Typed('#myname', {
+  strings: ['Victor A. Bonifácio', 'Victor A. Bonifácio'],
+  typeSpeed: 90,
+});
