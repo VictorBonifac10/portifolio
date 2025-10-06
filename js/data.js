72fd1a16b128b8992b68b4ca5b59ac83f525ba7c
victorbonifac10 <victor.alvesbonifacio@gmail.com>
2025-10-06 16:29:41 -0300

    Alteracao na exibicao dos projetos

---
+++ js/data.js
@@ -0,0 +1,57 @@
+export const projects = [
+    {
+        src: './assets/img/project01.png',
+        name: 'WL Motorcycle Aesthetics',
+        description: 'Desenvolvimento de uma Landing Page responsiva para um Lava Rápido de Motos.',
+        stacks: ['HTML5', 'CSS3', 'BootStrap', 'JavaScript'],
+        repository: 'https://github.com/VictorBonifac10/wl-motorcycle-aesthetics',
+        site: 'https://wlmotorcycleaesthetics.netlify.app/',
+        field: 'Front-End'
+    },
+    {
+        src: './assets/img/project02.png',
+        name: 'Sir Wallet Bank',
+        description: 'Desenvolvimento de uma Landing Page responsiva para serviços bancários com conversor monetário.',
+        stacks: ['HTML5', 'CSS3', 'BootStrap', 'TailwindCSS', 'JavaScript'],
+        repository: 'https://github.com/VictorBonifac10/business-website',
+        site: 'https://sirwalletbank.netlify.app/',
+        field: 'Front-End'
+    },
+    {
+        src: './assets/img/project03.png',
+        name: 'Cake House Confeitaria',
+        description: 'Desenvolvimento de uma Landing Page responsiva para serviços de uma confeitária, com carrinho de compras.',
+        stacks: ['HTML5', 'CSS3', 'BootStrap', 'TailwindCSS', 'JavaScript'],
+        repository: 'https://github.com/VictorBonifac10/candy-store',
+        site: 'https://cakehouseonline.netlify.app/',
+        field: 'Front-End'
+    },
+    {
+        src: './assets/img/project05.png',
+        name: 'Uptrend Marketing',
+        description: 'Desenvolvimento de uma Landing Page responsiva para serviços de uma agência de marketing.',
+        stacks: ['HTML5', 'CSS3', 'TailwindCSS', 'JavaScript'],
+        repository: 'https://github.com/VictorBonifac10/marketing-landing-page',
+        site: 'https://uptrendmarketing.netlify.app/',
+        field: 'Front-End'
+    },
+    {
+        src: './assets/img/project04.png',
+        name: 'Patient Manager',
+        description: 'Desenvolvimento de um sitema online, para a gestão de cadastro de pacientes.',
+        stacks: ['React', 'Styled-Components', 'Node.js', 'MongoDB'],
+        repository: 'https://patient-manager-xkq0.onrender.com/',
+        site: 'https://github.com/VictorBonifac10/patient-manager',
+        field: 'Full-Stack'
+    },
+    {
+        src: '',
+        name: 'Api Cadastro de Usuários',
+        description: 'Api',
+        stacks: ['Node.js'],
+        repository: 'https://patient-manager-xkq0.onrender.com/',
+        site: 'https://github.com/VictorBonifac10/patient-manager',
+        field: 'Back-End'
+    },
+
+];
--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+// --> Imports
+
+import { projects } from './data.js';
+
 AOS.init();
 
 // --> darkmode/lightmode
@@ -45,6 +49,65 @@ window.addEventListener("scroll", function () {
     header.classList.remove("scrolled");
   }
 });
+
+// --> projects
+
+const projectField = document.querySelector('.project-field')
+const btnAll = document.querySelector('#all');
+const btnFull = document.querySelector('#full-stack');
+const btnFront = document.querySelector('#front-end');
+const btnBack = document.querySelector('#back-end');
+
+function renderProjects(list) {
+
+  projectField.innerHTML = '';
+
+  list.forEach(element => {
+    const stacks = element.stacks.map(stack => `
+      <p class="text-[10px] text-amber-500 bg-amber-600/20 p-2 border-1 border-amber-600 rounded-md items-center justify-center">${stack}</p>
+    `).join('');
+
+    projectField.innerHTML += `
+      <article class="flex flex-col gap-3 w-full max-w-[390px] min-h-[150px] border-1 border-white/10 bg-gray-500/20 rounded-2xl hover:rounded-none hover:border-amber-400 hover:bg-amber-500/10 hover:scale-105 transition-all overflow-hidden">
+          <img src="${element.src}" alt="projeto"
+              class="w-auto sm:w-[150px] flex-shrink-0 object-cover">
+          <div class="flex flex-col justify-between flex-1 min-w-0 h-full p-3 pt-1 gap-2">
+              <h5 class="text-amber-400 font-semibold text-lg leading-tight">${element.name}</h5>
+              <p class="textToggle text-[13px] text-gray-300 mb-2">
+                  ${element.description}
+              </p>
+              <div class="flex flex-wrap gap-1">
+                  ${stacks}
+              </div>
+              <div class="technologies flex gap-3 mt-2 text-2xl text-amber-400">
+                  <a href="${element.repository}" target="_blank" class="flex-shrink-0"><i class="ri-github-fill" data-tooltip="Github"></i></a>
+                  <a href="${element.site}" target="_blank" class="flex-shrink-0"><i class="ri-link" data-tooltip="Site"></i></a>
+              </div>
+          </div>
+      </article>
+    `;
+  });
+}
+
+function filterProjects(field) {
+  if (field === 'Todos') {
+    renderProjects(projects);
+  } else {
+    const filtered = projects.filter(element => element.field === field);
+    renderProjects(filtered);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  filterProjects('Todos');
+});
+
+btnAll.addEventListener('click', () => filterProjects('Todos'));
+btnFull.addEventListener('click', () => filterProjects('Full-Stack'));
+btnFront.addEventListener('click', () => filterProjects('Front-End'));
+btnBack.addEventListener('click', () => filterProjects('Back-End'));
+
+
 
 
 
